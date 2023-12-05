c3a353a1917fca49aa0911f55fbecec862deb52d
Mostafa Kamel <mosatafaahmd54321@gmail.com>
2023-12-05 20:44:14 +0200

    start upload Assignment

---
+++ Unit_2_C^Programming/1-C_Basics^Assignments/EX1 ^ Print(Sentence).c
@@ -0,0 +1,17 @@
+/*
+ ======================================================================================================================
+ Name        : EX1 ^ Print(Sentence).c
+ Author      : Mostafa Kamel
+ Created on  : Dec 1, 2023
+ Description : Unit_2_C_Programming/1_C_Basics/Assignments/EX1_C_Program_To_Print_Sentence.c using Ansi-style
+ ======================================================================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	printf("C Programming");
+	return 0;
+}
