b6c12dd7731492d6fb21b4a71bd16ed87222b60c
Mostafa Kamel Elsoudy <82313475+Mostafa-Kamel-Soudy@users.noreply.github.com>
2023-12-06 13:33:06 +0200

    Update EX1 ^ Check whether a number even or odd.c

--- Unit_2^C_Programming/1-C_Basics^Assignments/Assignment2/EX1 ^ Check whether a number even or odd.c
+++ Unit_2^C_Programming/1-C_Basics^Assignments/Assignment2/EX1 ^ Check whether a number even or odd.c
@@ -1,24 +1,28 @@
 /*
  ======================================================================================================================
- Name        : Check whether a number even or odd.c
+ Name        : CheckEvenOrOdd.c
  Author      : Mostafa Kamel
  Created on  : Dec 5, 2023
- Description : Unit_2_C^Programming/C_Basics^Assignments/Assignment2/Check whether a number even or odd using Ansi-style
+ Description : A C program that prompts the user to enter an integer, checks whether it's even or odd, and prints the result using Ansi-style.
  ======================================================================================================================
  */
 
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
-{
-	int a;
-	printf("Enter an integer you want to check: ");
-	fflush(stdout);fflush(stdin);
-	scanf("%d",&a);
-	if((a%2) == 0)
-		printf("%d is even\n",a);
-	else
-		printf("%d is odd\n",a);
-	return 0;
+int main() {
+    int userInput;
+
+    printf("Enter an integer to check: ");
+    fflush(stdout);  // Flush the output buffer
+    fflush(stdin);   // Flush the input buffer
+    scanf("%d", &userInput);
+
+    if (userInput % 2 == 0) {
+        printf("%d is even\n", userInput);
+    } else {
+        printf("%d is odd\n", userInput);
+    }
+
+    return 0;
 }
