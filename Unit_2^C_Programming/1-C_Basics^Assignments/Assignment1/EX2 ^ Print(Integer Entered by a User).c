456b6b0a8f13d0b940755799afc7139d066c5f39
Mostafa Kamel Elsoudy <82313475+Mostafa-Kamel-Soudy@users.noreply.github.com>
2023-12-06 13:27:11 +0200

    Update EX2 ^ Print(Integer Entered by a User).c

--- Unit_2^C_Programming/1-C_Basics^Assignments/Assignment1/EX2 ^ Print(Integer Entered by a User).c
+++ Unit_2^C_Programming/1-C_Basics^Assignments/Assignment1/EX2 ^ Print(Integer Entered by a User).c
@@ -1,21 +1,23 @@
 /*
  ======================================================================================================================
- Name        : EX2 ^ Print(Integer Entered by a User).c
+ Name        : PrintUserEnteredInteger.c
  Author      : Mostafa Kamel
  Created on  : Dec 1, 2023
- Description : Unit_2_C^Programming/C_Basics^Assignments/Assignment1/EX2 ^ Print(Integer Entered by a User) using Ansi-style
+ Description : A C program that prompts the user to enter an integer and prints the entered value using Ansi-style.
  ======================================================================================================================
  */
 
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
-{
-	int x;
-	printf("Enter an integer: ");
-        fflush(stdout);fflush(stdin);
-	scanf("%d",&x);
-	printf("You entered %d",x);
-	return 0;
+int main() {
+    int userInput;
+
+    printf("Enter an integer: ");
+    fflush(stdout);  // Flush the output buffer
+    scanf("%d", &userInput);
+
+    printf("You entered %d\n", userInput);
+
+    return 0;
 }
