3f1f80933dc7a7ab19b966bed20d179cbd71d87d
Mostafa Kamel Elsoudy <82313475+Mostafa-Kamel-Soudy@users.noreply.github.com>
2023-12-06 13:29:17 +0200

    Update EX5 ^ Find ASCII Value of a Character.c

--- Unit_2^C_Programming/1-C_Basics^Assignments/Assignment1/EX5 ^ Find ASCII Value of a Character.c
+++ Unit_2^C_Programming/1-C_Basics^Assignments/Assignment1/EX5 ^ Find ASCII Value of a Character.c
@@ -1,21 +1,24 @@
 /*
  ======================================================================================================================
- Name        : EX5 ^ Find ASCII Value of a Character.c
+ Name        : FindASCIIValueOfCharacter.c
  Author      : Mostafa Kamel
  Created on  : Dec 1, 2023
- Description : Unit_2_C^Programming/C_Basics^Assignments/Assignment1/EX5 ^ Find ASCII Value of a Character using Ansi-style
+ Description : A C program that prompts the user to enter a character, finds its ASCII value, and prints the result using Ansi-style.
  ======================================================================================================================
  */
 
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
-{
-	char c;
-	printf("Enter a character: ");
-	fflush(stdout);fflush(stdin);
-	scanf("%c",&c);
-	printf("ASCII Value of %c is %d",c,c);
-	return 0;
+int main() {
+    char inputCharacter;
+
+    printf("Enter a character: ");
+    fflush(stdout);  // Flush the output buffer
+    fflush(stdin);   // Flush the input buffer
+    scanf("%c", &inputCharacter);
+
+    printf("ASCII Value of %c is %d\n", inputCharacter, inputCharacter);
+
+    return 0;
 }
