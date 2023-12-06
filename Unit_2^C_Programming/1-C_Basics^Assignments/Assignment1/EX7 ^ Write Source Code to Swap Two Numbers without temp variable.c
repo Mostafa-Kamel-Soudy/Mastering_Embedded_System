bab99a4bc555e1a7e2e34fd71d17df4be27629e2
Mostafa Kamel Elsoudy <82313475+Mostafa-Kamel-Soudy@users.noreply.github.com>
2023-12-06 13:31:18 +0200

    Update EX7 ^ Write Source Code to Swap Two Numbers without temp variable.c

--- Unit_2^C_Programming/1-C_Basics^Assignments/Assignment1/EX7 ^ Write Source Code to Swap Two Numbers without temp variable.c
+++ Unit_2^C_Programming/1-C_Basics^Assignments/Assignment1/EX7 ^ Write Source Code to Swap Two Numbers without temp variable.c
@@ -1,29 +1,35 @@
 /*
  =================================================================================================================================================
- Name        : EX7 ^ Write Source Code to Swap Two Numbers without temp variable.c
+ Name        : SwapTwoNumbersWithoutTemp.c
  Author      : Mostafa Kamel
  Created on  : Dec 1, 2023
- Description : Unit_2_C^Programming/C_Basics^Assignments/Assignment1/EX7 ^ Write Source Code to Swap Two Numbers without temp variable using Ansi-style
+ Description : A C program that prompts the user to enter two numbers, swaps them without using a temporary variable, and prints the swapped values using Ansi-style.
  ==================================================================================================================================================
  */
 
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
-{
-	float a,b;
-	printf("Enter a value of a: ");
-	fflush(stdout);fflush(stdin);
-	scanf("%f",&a);
-	printf("Enter a value of b: ");
-	fflush(stdout);fflush(stdin);
-	scanf("%f",&b);
-	a=a+b;
-	b=a-b;
-	a=a-b;
-	printf("After swapping, Value of a: %.2f\n",a);
-	printf("After swapping, Value of b: %.2f\n",b);
+int main() {
+    float num1, num2;
 
-	return 0;
+    printf("Enter the value of num1: ");
+    fflush(stdout);  // Flush the output buffer
+    fflush(stdin);   // Flush the input buffer
+    scanf("%f", &num1);
+
+    printf("Enter the value of num2: ");
+    fflush(stdout);  // Flush the output buffer
+    fflush(stdin);   // Flush the input buffer
+    scanf("%f", &num2);
+
+    // Swapping without temp variable
+    num1 = num1 + num2;
+    num2 = num1 - num2;
+    num1 = num1 - num2;
+
+    printf("After swapping, Value of num1: %.2f\n", num1);
+    printf("After swapping, Value of num2: %.2f\n", num2);
+
+    return 0;
 }
