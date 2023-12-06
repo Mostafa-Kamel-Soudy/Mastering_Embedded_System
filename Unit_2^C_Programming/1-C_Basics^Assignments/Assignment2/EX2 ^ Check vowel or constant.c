272cc7b5a113d0418811ef50bf289910937a517a
Mostafa Kamel Elsoudy <82313475+Mostafa-Kamel-Soudy@users.noreply.github.com>
2023-12-06 13:33:58 +0200

    Update EX2 ^ Check vowel or constant.c

--- Unit_2^C_Programming/1-C_Basics^Assignments/Assignment2/EX2 ^ Check vowel or constant.c
+++ Unit_2^C_Programming/1-C_Basics^Assignments/Assignment2/EX2 ^ Check vowel or constant.c
@@ -1,24 +1,30 @@
 /*
  ======================================================================================================================
- Name        : EX2 ^ Check vowel or constant.c
+ Name        : CheckVowelOrConsonant.c
  Author      : Mostafa Kamel
  Created on  : Dec 5, 2023
- Description : Unit_2_C^Programming/C_Basics^Assignments/Assignment2/EX2 ^ Check vowel or constant using Ansi-style
+ Description : A C program that prompts the user to enter an alphabet, checks whether it's a vowel or consonant, and prints the result using Ansi-style.
  ======================================================================================================================
  */
 
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
-{
-	char c;
-	printf("Enter an alphabet: ");
-	fflush(stdout);fflush(stdin);
-	scanf("%c",&c);
-	if(c=='A'||c=='a'||c=='E'||c=='e'||c=='I'||c=='i'||c=='O'||c=='o'||c=='U'||c=='u')
-		printf("%c is vowel\n",c);
-	else
-		printf("%c is constant\n",c);	
-        return 0;
+int main() {
+    char userInput;
+
+    printf("Enter an alphabet: ");
+    fflush(stdout);  // Flush the output buffer
+    fflush(stdin);   // Flush the input buffer
+    scanf("%c", &userInput);
+
+    if (userInput == 'A' || userInput == 'a' || userInput == 'E' || userInput == 'e' ||
+        userInput == 'I' || userInput == 'i' || userInput == 'O' || userInput == 'o' ||
+        userInput == 'U' || userInput == 'u') {
+        printf("%c is a vowel\n", userInput);
+    } else {
+        printf("%c is a consonant\n", userInput);
+    }
+
+    return 0;
 }
